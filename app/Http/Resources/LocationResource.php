18a1d7049c522b0522e8cc1c9c1eef79a70aea95
Salehgoied <salehgoied@gmail.com>
2024-11-14 16:36:25 +0300

    test locations

--- app/Http/Resources/LocationResource.php
+++ app/Http/Resources/LocationResource.php
@@ -34,7 +34,7 @@ class LocationResource extends JsonResource
             'slug' => $this->slug,
             'map' => $this->map,
             'country_name' => LocaleHelper::getLocalizedField($this->country, 'name'),
-            'city_id' => $this->city->id,
+            // 'city_id' => $this->city->id,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
